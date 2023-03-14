ff2bb5d48358f3dde5b9ca7cf039000fdf3688d2
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-03-14 13:11:17 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -79,10 +79,57 @@ namespace fans
 
   public class FA2
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
+    public static State q0 = new State()
+        {
+            Name = "q0",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q1 = new State()
+        {
+            Name = "q1",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q2 = new State()
+        {
+            Name = "q2",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q3 = new State()
+        {
+            Name = "q3",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState = q0;
+
+        public FA2()
+        {
+            q0.Transitions['0'] = q1;
+            q0.Transitions['1'] = q3;
+            q1.Transitions['0'] = q0;
+            q1.Transitions['1'] = q2;
+            q2.Transitions['0'] = q3;
+            q2.Transitions['1'] = q1;
+            q3.Transitions['0'] = q2;
+            q3.Transitions['1'] = q0;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s) // цикл по всем символам 
+            {
+                current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+                if (current == null)              // если его нет, возвращаем признак ошибки
+                    return null;
+                // иначе переходим к следующему
+            }
+            return current.IsAcceptState;         // результат true если в конце финальное состояние 
+        }
   }
   
   public class FA3
